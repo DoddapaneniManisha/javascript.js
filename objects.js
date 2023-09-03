568e215dfea748d7697070886d5d00e364447d84
DoddapaneniManisha <139426051+DoddapaneniManisha@users.noreply.github.com>
2023-09-03 16:26:57 +0530

    Create objects.js

---
+++ objects.js
@@ -0,0 +1,15 @@
+var student ={
+    name : "manisha",
+    phoneNumber : "9492378604",
+    marks : {
+        math : 76,
+        science : 80,
+        english : 90
+    }
+}
+console.log("student name:"+ student.name);
+console.log("phoneNumber:"+student.phoneNumber);
+console.log("marks");
+console.log("math:"+student.marks.math);
+console.log("science:"+student.marks.science);
+console.log("english:"+student.marks.english);
