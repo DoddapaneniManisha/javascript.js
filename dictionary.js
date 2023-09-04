556959ce510431c3bb9fe4d3889c7cccf2d72d05
DoddapaneniManisha <139426051+DoddapaneniManisha@users.noreply.github.com>
2023-09-04 11:16:02 +0530

    Create dictionary.js

---
+++ dictionary.js
@@ -0,0 +1,8 @@
+var wordDictionary = {
+    "apple" : " a round fruit with red or green skin and crisp flesh ",
+    "computer" : " an electronic device that can store,process,and retieve data",
+     "book" : "a written or printed work consisting of pages glued or sewn together along ine side" 
+}
+console.log("meaning of'apple': "+wordDictionary["apple"]);
+console.log("meaning of 'computer': "+wordDictionary["computer"]);
+console.log("meaning of 'book': "+ wordDictionary["book"]);
