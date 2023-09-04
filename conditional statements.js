353469e2cf8b3dad1014eaf8a3f29c34ede6709e
DoddapaneniManisha <139426051+DoddapaneniManisha@users.noreply.github.com>
2023-09-04 12:20:08 +0530

    Create conditional statements.js

---
+++ conditional statements.js
@@ -0,0 +1,8 @@
+var age =15;
+var isBetween10and20 =age>=10 && age<=20;
+if(isBetween10and20)
+{
+    console.log("the age is between 10 and 20");
+}else{
+    console.log("teh age is not between 10 and 20");
+}
